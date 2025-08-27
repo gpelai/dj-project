1aea2a297aaad67100b3b8807aafe58c52263e67
Gabriel Pelai <gabrielpelaicontato@gmail.com>
2025-08-26 21:10:10 -0300

    fix: jinja/django layout curriculo

--- curriculo/admin.py
+++ curriculo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Certificados, Experiencias, Skills
 # Register your models here.
+
+admin.site.register(Certificados)
+admin.site.register(Experiencias)
+admin.site.register(Skills)
--- curriculo/views.py
+++ curriculo/views.py
@@ -1,21 +1,26 @@
 from django.shortcuts import render, get_object_or_404
-from curriculo.models import Certificados, Experiencias
+from curriculo.models import Certificados, Experiencias, Skills
 
 # Create your views here.
 def curriculo(request):
     certificados = Certificados.objects.all()
     experiencias = Experiencias.objects.all()
+    skills = Skills.objects.all()
+
     context = {
         "certificados": certificados,
         "experiencias": experiencias,
+        "skills": skills,
     }
     return render(request, "curriculo/index.html", context)
 
 def curriculo_detail(request, pk):
     certificado = get_object_or_404(Certificados, pk=pk)
     experiencia = get_object_or_404(Experiencias, pk=pk)
+    skills = get_object_or_404(Skills, pk=pk)
     context = {
         "certificado": certificado,
         "experiencia": experiencia,
+        "skills": skills,
     }
     return render(request, "curriculo/detail.html", context)
